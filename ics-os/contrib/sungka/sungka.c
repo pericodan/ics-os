16c44c015d59550460f64d61f57ab621a3036199
pericodan <perico.dionisio@gmail.com>
2016-05-07 16:38:52 +0800

    Draw board

--- ics-os/contrib/sungka/sungka.c
+++ ics-os/contrib/sungka/sungka.c
@@ -5,6 +5,9 @@
 #define down_key 's'
 #define right_key 'd'
 #define left_key 'a'
+#define enter '\n'
+#define quit 27
+
 
 #define YELLOW 54
 #define GRAY 56
@@ -21,12 +24,16 @@
 #define DARK_GREEN 16
 #define DARK_BROWN 32
 
+char player1[50];
+char player2[50];
+
+int items[16] = {0, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 0};
 
 /*
     "Erases" the screen given the starting point and the width & height
     (from the blacjack application in the ics-os)
 */
-void erase(int x, int y, int w, int h, int color){
+void drawRectangle(int x, int y, int w, int h, int color){
    int i,j;
    for (i=y;i<=(y+h);i++)
       for (j=x;j<=(x+w);j++)
@@ -34,16 +41,84 @@ void erase(int x, int y, int w, int h, int color){
 }
 
 void drawMenu(){
+    drawRectangle(0,0,320,220, BLACK);
     write_text("SUNGKA",41,41,WHITE,1);
 
-    write_text("[Enter]Start",40,120,WHITE,0); 
+    write_text("[Enter]Start",40,120,WHITE,0);
 	write_text("[Esc]Quit",40,140,WHITE,0);
 	write_text("[i]Instructions", 40, 160, WHITE, 0);
 }
 
-main(){
+void getNames(){
+/*    int i;
+    char temp[50];
 
+    drawRectangle(0,0,320,220, BLACK);
+    write_text("Enter name of player 1",41,41,WHITE,1);
+    write_text("Press Enter to proceed",41,100,WHITE,1);
+
+    scanf("%s", temp);
+    for(i=41;;i+=10){
+        //fgets() how to fgets po
+        write_text(temp+"",i,61,WHITE,0);
+    }
+
+    drawRectangle(0,0,320,220, BLACK);
+    write_text("Enter name of player 2",41,41,WHITE,1);
+*/
+
+}
+
+void drawBoard(){
+    int x, z;
+    char temp[1];
+    drawRectangle(0,0,320,220, BLACK);
+
+    drawRectangle(4,70,30,20, BROWN);
+    sprintf(temp, "%d", items[0]);
+    write_text(temp,12,75,WHITE,0);
+
+    z=1;
+    for(x=39; x<270; x+=35){
+        drawRectangle(x,40,30,20, BROWN);
+        sprintf(temp, "%d", items[(x-4)/35]);
+        write_text(temp,x+8,45,WHITE,0);
+
+    }
+
+    z=8;
+    for(x=39; x<270; x+=35){
+        drawRectangle(x,100,30,20, DARK_BROWN);
+        sprintf(temp, "%d", items[((x-4)/35)+7]);
+        write_text(temp,x+8,105,WHITE,0);
+
+    }
+
+    drawRectangle(285,70,30,20, DARK_BROWN);
+    sprintf(temp, "%d", items[15]);
+    write_text(temp,293,75,WHITE,0);
+
+}
+
+void startGame(){
+    //getNames();
+    drawBoard();
+
+}
+
+main(){
+    char pressed;
     set_graphics(VGA_320X200X256);
-    erase(0,0,320,220, BLACK);
     drawMenu();
+
+    pressed = (char)getch();
+    //do{
+        if(pressed==enter){
+            startGame();
+        }
+        else if(pressed=='i'){
+            //instruction();
+        }
+    //}while(pressed!=quit);
+
 }
